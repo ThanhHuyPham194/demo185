7cfacc1edfe935bcff017e05319c8b70f1d93817
DAI QUOC VIET <daiquocviet@DAIs-MacBook-Pro.local>
2022-11-06 23:52:27 +0700

    margin

--- src/pages/Relic/RelicHeading/RelicHeading.jsx
+++ src/pages/Relic/RelicHeading/RelicHeading.jsx
@@ -15,10 +15,10 @@ export default function RelicHeading() {
       <span className="relicHeading__text">Di tích</span>
       <span className="relicHeading__name">Lăng Lê Văn Duyệt</span>
       <div className="relicHeading__image">
-      <div className="unity">
+      {/* <div className="unity"> */}
           {/* <Unity unityProvider={unityProvider} style={{ width: '100%', height: '100%' }} /> */}
           {/* <img src={lang} alt='lang'/> */}
-      </div>
+      {/* </div> */}
       <video className='haha' autoPlay loop >
             <source src={lang} type='video/mp4'/>
           </video>
